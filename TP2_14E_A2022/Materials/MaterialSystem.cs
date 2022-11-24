881ab37e5607fd86c9d790a2e87fe87f41b8f58c
Hugo Bellemare-Vallieres <hbvallieres@ova.ai>
2022-11-24 14:30:46 -0500

    Fixed tests

--- TP2_14E_A2022/Materials/MaterialSystem.cs
+++ TP2_14E_A2022/Materials/MaterialSystem.cs
@@ -15,8 +15,7 @@ namespace TP2_14E_A2022.Materials
 
         public static Material? AddMaterial(string name, string description)
         {
-            return null;
-            /*if(name == string.Empty)
+            if (name == string.Empty)
             {
                 return null;
             }
@@ -24,7 +23,7 @@ namespace TP2_14E_A2022.Materials
             {
                 Material material = new Material(name, description);
                 return materialDal.AddMaterial(material);
-            }*/
+            }
         }
 
         public static List<Material> GetMaterials()
