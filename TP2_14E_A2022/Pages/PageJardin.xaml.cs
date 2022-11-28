5a73d0983e066b95d1fbb815cce1040c76d029a0
Simon Leger <regelnomis@hotmail.com>
2022-11-28 15:42:53 -0500

    Ajout Frontend + ressources

---
+++ TP2_14E_A2022/Pages/PageJardin.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TP2_14E_A2022.Pages
+{
+    /// <summary>
+    /// Logique d'interaction pour PageJardin.xaml
+    /// </summary>
+    public partial class PageJardin : Page
+    {
+        public PageJardin()
+        {
+            InitializeComponent();
+        }
+    }
+}
