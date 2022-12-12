7d4b7939428b1cc6cc9a1fce92b0b82974f725bb
Simon Leger <regelnomis@hotmail.com>
2022-12-12 01:03:47 -0500

    Added the function to show all gardens with correct image

--- TP2_14E_A2022/Pages/PageGarden.xaml.cs
+++ TP2_14E_A2022/Pages/PageGarden.xaml.cs
@@ -12,8 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TP2_14E_A2022.Lots;
 using TP2_14E_A2022.Materials;
 using TP2_14E_A2022.Users;
+using TP2_14E_A2022.DataModels;
 
 namespace TP2_14E_A2022.Pages
 {
@@ -22,6 +24,11 @@ namespace TP2_14E_A2022.Pages
         public PageGarden()
         {
             InitializeComponent();
+            FetchMaterials();
+            FetchMembers();
+            FetchGarden();
+            listViewMaterials.SelectedIndex = 0;
+            listViewMembers.SelectedIndex = 0;
             tbUser.Text = UserSystem.GetConnectedUserName();
         }
         
@@ -50,12 +57,16 @@ namespace TP2_14E_A2022.Pages
 
         private void Button_Remove_Member(object sender, RoutedEventArgs e)
         {
+            // TODO : remove user from lot
 
+            FetchGarden();
         }
 
         private void Button_Add_Member(object sender, RoutedEventArgs e)
         {
+            // TODO : add user to lot
 
+            FetchGarden();
         }
 
         private void FetchMaterials()
@@ -68,5 +79,34 @@ namespace TP2_14E_A2022.Pages
             listViewMembers.ItemsSource = UserSystem.GetUsers();
         }
 
+        private void FetchGarden()
+        {
+            List<Lot> lots = LotSystem.GetLots();
+            Image[] lotImages = { Lot0, Lot1, Lot2, Lot3, Lot4, Lot5, Lot6, Lot7, Lot8, Lot9, Lot10, Lot11, Lot12, Lot13, Lot14, Lot15, Lot16, Lot17, Lot18, Lot19 };
+            foreach (Lot lot in lots)
+            {
+                if (lot.state == LotState.Active)
+                {
+                    lotImages[lot.lotNumber].Source = new BitmapImage(new Uri("../Ressources/active-lot.png", UriKind.Relative));
+                }
+                else if (lot.state == LotState.Inactive)
+                {
+                    lotImages[lot.lotNumber].Source = new BitmapImage(new Uri("../Ressources/inactive-lot.png", UriKind.Relative));
+                }
+                else if (lot.state == LotState.Wintered)
+                {
+                    lotImages[lot.lotNumber].Source = new BitmapImage(new Uri("../Ressources/wintered-lot.png", UriKind.Relative));
+                }
+            }
+        }
+
+        private void Button_Winter_Lots(object sender, RoutedEventArgs e)
+        {
+            List<Lot> lots = LotSystem.GetLots();
+            foreach (Lot lot in lots)
+            {
+                LotSystem.WinterLot(lot.lotNumber);
+            }
+        }
     }
 }
